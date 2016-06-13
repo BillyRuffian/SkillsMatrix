7fd1016825973dd1d14995c4299fa39f935a1cad
Nigel Brookes-Thomas <nigel.brookes-thomas@digital.dvla.gov.uk>
2016-06-13 15:27:08 +0100

    removed Test::Unit tests

--- test/controllers/admin/team_memberships_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class Admin::TeamMembershipsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get admin_team_memberships_index_url
-    assert_response :success
-  end
-
-end
--- test/mailers/previews/admin_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/admin_mailer
-class AdminMailerPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/admin_mailer/new_registration_email
-  def new_registration_email
-    AdminMailer.new_registration_email
-  end
-
-end
