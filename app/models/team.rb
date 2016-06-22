5ef1df1be3581f58b2df2f2ebe83ecf171878bd9
Nigel Brookes-Thomas <nigel.brookes-thomas@digital.dvla.gov.uk>
2016-06-22 16:20:38 +0100

    bug fix team model to include leader foreign_key

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,7 +2,7 @@ class Team < ApplicationRecord
   has_and_belongs_to_many :skills
   has_and_belongs_to_many :users
 
-  belongs_to :leader, class_name: 'User', inverse_of: :leads
+  belongs_to :leader, class_name: 'User', inverse_of: :leads, foreign_key: :user_id
 
   validates_presence_of :name
   validates_uniqueness_of :name
